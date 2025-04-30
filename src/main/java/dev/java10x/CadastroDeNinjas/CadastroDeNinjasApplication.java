78d002918a9c6afef7f3613412064a57d77592d9
KingDanone <lricardolv10@gmail.com>
2025-04-30 12:13:18 -0300

    Criado a Controller [AL2]

--- src/main/java/dev/java10x/CadastroDeNinjas/CadastroDeNinjasApplication.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/CadastroDeNinjasApplication.java
@@ -2,6 +2,7 @@ package dev.java10x.CadastroDeNinjas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class CadastroDeNinjasApplication {
@@ -9,5 +10,4 @@ public class CadastroDeNinjasApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CadastroDeNinjasApplication.class, args);
 	}
-
 }
